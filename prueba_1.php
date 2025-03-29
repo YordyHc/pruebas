23eb4cd4bffd8f51b811d8b8fd59cefde7f5e128
YordyHc <yordyhuamanc@gmail.com>
2025-03-29 13:56:57 -0500

    agregando html

--- prueba_1.php
+++ prueba_1.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Div Desplazable</title>
+    <title>Galería de YouTube</title>
 </head>
 <body>
-    <!-- Elfsight YouTube Gallery | Untitled YouTube Gallery -->
-<script src="https://static.elfsight.com/platform/platform.js" async></script>
-<div class="elfsight-app-0b6c46f0-bc43-4fe0-8a5d-7173d0fd9873" data-elfsight-app-lazy></div>
+    <h1>Galería de Videos de YouTube</h1>
+
+    <!-- Contenedor donde se insertarán los videos -->
+    <div id="youtube-gallery"></div>
+
+    <!-- Incluir el archivo JavaScript -->
+    <script src="https://yordyhc.github.io/pruebas/script/script.js"></script>
 </body>
 </html>
